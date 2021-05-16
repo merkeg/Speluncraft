a247a0f4dc140ff35664519cfc9cfa44e199ccc8
Muellermar <mm-192026@rwu.de>
2021-05-16 21:11:58 +0200

    quick fix

--- Game/UI/HealthbarPlayer.cs
+++ Game/UI/HealthbarPlayer.cs
@@ -11,8 +11,8 @@ namespace Game.UI
     using Engine.Component;
     using Engine.GameObject;
     using Engine.Renderer;
-    using Game.Gun;
     using Engine.Renderer.Sprite;
+    using Game.Gun;
     using OpenTK.Graphics.OpenGL;
     using OpenTK.Mathematics;
     using OpenTK.Windowing.Common;
