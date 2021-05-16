33f2747a4a948bedcf643c8d72c21a975b2e469f
Muellermar <mm-192026@rwu.de>
2021-05-16 17:38:00 +0200

    working background render

--- Game/UI/HealthbarPlayer.cs
+++ Game/UI/HealthbarPlayer.cs
@@ -23,17 +23,21 @@ namespace Game.UI
     public class HealthbarPlayer : IRenderer
     {
         private static int currentHP = 0;
-        private static float uiScale = 1.5f;
+        private static float uiScale = 1.0f;
 
         private static float xOffset = 25;
         private static float yOffset = 25;
         private static int screenX;
         private static int screenY;
 
+        private static int backgroundXSize = 300;
+        private static int backgroundYSize = 60;
+
         private static HealthbarPlayer instance;
         private static Player player;
         private Assembly assembly;
-        private Sprite sprite;
+        private Sprite indicators;
+        private Sprite background;
         private float hTexX0;
 
         /// <summary>
@@ -53,11 +57,11 @@ namespace Game.UI
             }
 
             this.assembly = Assembly.GetExecutingAssembly();
-            using Stream spriteStream = this.assembly.GetManifestResourceStream("Game.Resources.Sprite.UI.Healthbar.heartsheet_new.png");
-            this.sprite = new Sprite(spriteStream, false);
+            using Stream indicatorSpriteStream = this.assembly.GetManifestResourceStream("Game.Resources.Sprite.UI.Healthbar.heartsheet_new.png");
+            this.indicators = new Sprite(indicatorSpriteStream, false);
 
-            // using Stream backgroundStream = this.assembly.GetManifestResourceStream("Game.Resources.Sprite.UI.Healthbar.ui_background.png");
-            // this.uiBackground = new Sprite(backgroundStream, false);
+            using Stream backgroundSpriteStream = this.assembly.GetManifestResourceStream("Game.Resources.Sprite.UI.Healthbar.background2.png");
+            this.background = new Sprite(backgroundSpriteStream, false);
         }
 
         /// <summary>
@@ -80,12 +84,14 @@ namespace Game.UI
         /// <param name="args">.</param>
         public void Render(FrameEventArgs args)
         {
-            currentHP = player.GetComponent<HealthPoints>().GetCurrHP();
 
+            this.RenderBackground();
+
+            currentHP = player.GetComponent<HealthPoints>().GetCurrHP();
             this.hTexX0 = currentHP / 100f;
 
             // heart outline
-            GL.BindTexture(TextureTarget.Texture2D, this.sprite.Handle);
+            GL.BindTexture(TextureTarget.Texture2D, this.indicators.Handle);
             GL.Color4(new Color4(1.0f, 1.0f, 1.0f, 1.0f));
             GL.Begin(PrimitiveType.Quads);
 
@@ -104,7 +110,7 @@ namespace Game.UI
             GL.End();
 
             // heart inlay
-            GL.BindTexture(TextureTarget.Texture2D, this.sprite.Handle);
+            GL.BindTexture(TextureTarget.Texture2D, this.indicators.Handle);
             GL.Color4(new Color4(1.0f, 0f, 0f, 1.0f)); // change color of inlay here
             GL.Begin(PrimitiveType.Quads);
 
@@ -123,6 +129,55 @@ namespace Game.UI
             GL.End();
 
             // Debug.WriteLine("Y0: " + this.hTexY0 + "hTexY1: " + this.hTexY1);
+        }
+
+        /// <summary>
+        /// function to render healthbar background.
+        /// </summary>
+        public void RenderBackground()
+        {
+            // healthbar background
+            GL.BindTexture(TextureTarget.Texture2D, this.background.Handle);
+            GL.Color4(new Color4(1.0f, 1.0f, 1.0f, 1.0f));
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(xOffset * uiScale, screenY - yOffset - (60 * uiScale));
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(backgroundXSize * uiScale, screenY - yOffset - (60 * uiScale));
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(backgroundXSize * uiScale, screenY - yOffset);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(xOffset, screenY - yOffset);
+
+            GL.End();
+        }
+
+        /// <summary>
+        /// function to render indicators of healthbar.
+        /// </summary>
+        public void RenderIndicators(Color4 color)
+        {
+            GL.BindTexture(TextureTarget.Texture2D, this.indicators.Handle);
+            GL.Color4(color);
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(0, 1f / 20f);
+            GL.Vertex2(xOffset, yOffset);
+
+            GL.TexCoord2(1, 1f / 20f);
+            GL.Vertex2(xOffset + (220 * uiScale), yOffset);
+
+            GL.TexCoord2(1, 1f / 20f * 2f);
+            GL.Vertex2(xOffset + (220 * uiScale), yOffset + (18 * uiScale));
+
+            GL.TexCoord2(0, 1f / 20f * 2f);
+            GL.Vertex2(xOffset, yOffset + (18 * uiScale));
+
+            GL.End();
         }
 
         /// <summary>
