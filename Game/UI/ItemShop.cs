9eb169dcb616c4c84f3d6049523da9016fc819a4
Muellermar <mm-192026@rwu.de>
2021-05-20 08:24:21 +0200

    Main Class Structure build

---
+++ Game/UI/ItemShop.cs
@@ -0,0 +1,64 @@
+﻿// <copyright file="ItemShop.cs" company="RWUwU">
+// Copyright (c) RWUwU. All rights reserved.
+// </copyright>
+
+namespace Game.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Engine.GameObject;
+    using Engine.Renderer;
+    using Engine.Renderer.Particle;
+    using Engine.Renderer.Sprite;
+    using Engine.Renderer.Text;
+    using Engine.Renderer.Tile;
+    using Engine.Renderer.UI;
+    using Engine.Service;
+    using Game.Gun;
+    using OpenTK.Mathematics;
+    using OpenTK.Windowing.Common;
+    using OpenTK.Windowing.GraphicsLibraryFramework;
+
+    /// <summary>
+    /// Class for Item Shop ingame.
+    /// </summary>
+    public class ItemShop : IUpdatable, IRenderer
+    {
+        /// <inheritdoc/>
+        public void OnRendererCreate()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void OnUpdatableCreate()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void OnUpdatableDestroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void OnUpdate(float frameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Render(FrameEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Resize(ResizeEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
