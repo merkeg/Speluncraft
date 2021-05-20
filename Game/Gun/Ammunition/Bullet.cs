5ba84fae7a7ba4a7b0c80bc45580e28613024ee4
Tobias Niedermaier <tobias.niedermaier@rwu.de>
2021-05-20 11:59:49 +0200

    Bullets can now fly not straight.

--- Game/Gun/Ammunition/Bullet.cs
+++ Game/Gun/Ammunition/Bullet.cs
@@ -20,21 +20,22 @@ namespace Game.Gun.Ammunition
         /// Initializes a new instance of the <see cref="Bullet"/> class.
         /// </summary>
         /// <param name="dmg">How much dmg it will do on impact.</param>
-        /// <param name="velocityX">How fast it is flying.</param>
+        /// <param name="velocityX">How fast it is flying straight.</param>
+        /// <param name="velocityY">How fast it is flying up.</param>
         /// <param name="minX">Where it will spawn on X-Axsis.</param>
         /// <param name="minY">Where it will spawn on Y-Axsis.</param>
         /// <param name="sizeX">Size X.</param>
         /// <param name="sizeY">Size Y.</param>
         /// <param name="sprite">Its sprite.</param>
         /// <param name="damageDelayFrames">How many frames to wait, before this bullet will do Damage.</param>
-        public Bullet(int dmg, float velocityX, float minX, float minY, float sizeX, float sizeY, Engine.Renderer.Sprite.ISprite sprite, int damageDelayFrames)
+        public Bullet(int dmg, float velocityX, float velocityY, float minX, float minY, float sizeX, float sizeY, Engine.Renderer.Sprite.ISprite sprite, int damageDelayFrames)
             : base(minX, minY, sizeX, sizeY, sprite)
         {
             this.damageDelay = damageDelayFrames;
             this.dmg = dmg;
             Engine.Component.Physics p = new Engine.Component.Physics();
-            p.SetVelocity(velocityX, 0);
-            p.SetMaxVelocity(Math.Abs(velocityX), 0);
+            p.SetVelocity(velocityX, velocityY);
+            p.SetMaxVelocity(Math.Abs(velocityX), velocityY);
             p.SetIsAffectedByGravity(false);
             this.AddComponent(p);
         }
